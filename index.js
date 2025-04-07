0b41f53dbe8b4a7ccf0f6c6b4363479da48453b7
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-04-07 13:15:01 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-// API: Retrieve Students Above Threshold
-// ---------------------------------------
-// Task:
-// Implement an API to fetch students whose total marks exceed a given threshold.
-//
-// Endpoint:
-// POST /students/above-threshold
-//
-// Request Body:
-// {
-//   "threshold": <number>
-// }
-//
-// Response:
-// Success: List of students with their names and total marks who meet the criteria.
-// Example:
-// {
-//   "count": 2,
-//   "students": [
-//     { "name": "Alice Johnson", "total": 433 },
-//     { "name": "Bob Smith", "total": 410 }
-//   ]
-// }
-//
-// No Matches:
-// {
-//   "count": 0,
-//   "students": []
-// }
-//
-// Purpose:
-// Help teachers retrieve and analyze student performance efficiently.
-
-
 const express = require('express');
-const { resolve } = require('path');
+const path = require('path');
+const fs = require('fs')
 
 const app = express();
 const port = 3010;
-
 app.use(express.static('static'));
+app.use(express.json()); 
+
+const rawData = fs.readFileSync('data.json');
+const studentsData = JSON.parse(rawData);
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+app.post('/students/above-threshold',async(req,res)=>{
+  
+  try{
+  const {threshold} = req.body;
+  
+  if (typeof threshold !== 'number') {
+    return res.status(400).json({
+      message: 'Invalid threshold. Please provide a number.'
+    });
+  }
+
+  const filteredStudents = studentsData
+    .map(student => {
+      const total = student.marks.reduce((sum, mark) => sum + mark, 0);
+      return { name: student.name, total };
+    })
+
+    .filter(student => student.total > threshold);
+
+  return res.json({
+    count: filteredStudents.length,
+    students: filteredStudents
+  });
+
+}
+  catch(error){
+
+    return res.status(500).json({error:"An Error Has Occured!"})
+  }
+
+})
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
